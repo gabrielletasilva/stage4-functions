df10d17ac9657801d804dcdf2975855da9fda443
Gabrielle <82898190+gabrielletasilva@users.noreply.github.com>
2023-12-13 18:00:40 -0300

    Add files via upload

---
+++ functions/functions1.js
@@ -0,0 +1,18 @@
+// criar um aplicativo de frases motivacionais 
+
+// declaration - declaração da função
+// function statement 
+function createPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é a mãe do aprendizado')
+}
+// agrupar um pouco de código, para usar depois ******
+// dar significado ****
+
+//executar a função
+// rodar, chamar, invocar
+// execute, run, call, invoke
+createPhrases()
+// reutilizar quantas vezes eu quiser ******
+console.log('fim do programa')
